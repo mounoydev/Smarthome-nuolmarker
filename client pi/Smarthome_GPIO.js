59704e14e82fafb7e7a0edee93d3403ffdf55126
pigletkps <pigletkps@hotmail.com>
2015-11-01 14:54:24 +0700

    Update Smarthome_GPIO.js

--- client pi/Smarthome_GPIO.js
+++ client pi/Smarthome_GPIO.js
@@ -1,6 +1,7 @@
-﻿//ປະກາດຄ່າ
+//ປະກາດຄ່າ
 var socket = require('socket.io-client')('http://mounoydev.com:3000/');
 var gpio = require("gpio");
+//ປະກາດຂາໄຟ
 var gpio4  = gpio.export(4, {direction: "out",ready: function(){gpio4.set(1); }});
 var gpio17 = gpio.export(17, {direction: "out",ready: function(){gpio17.set(1); }});
 var gpio18 = gpio.export(18, {direction: "out",ready: function(){gpio18.set(1); }});
@@ -18,12 +19,10 @@ var gpio11 = gpio.export(11, {direction: "out",ready: function(){gpio11.set(1); 
 var gpio2 = gpio.export(2, {direction: "out",ready: function(){gpio2.set(1); }});
 var gpio3 = gpio.export(3, {direction: "out",ready: function(){gpio3.set(1); }});
 var gpio14 = gpio.export(14, {direction: "out",ready: function(){gpio14.set(1); }});
-//var gpio21 = gpio.export(21, {direction: "out",ready: function(){gpio21.set(1); }});
 
 //ຕໍ່Server
 socket.on('connect', function(){
 console.log('connect');
-    //socket.emit('restore',{'pin':'4'});
     //ຕໍ່Server ເຂົ້າ Pi
 socket.emit('pi','');
 });
@@ -31,8 +30,8 @@ socket.emit('pi','');
 //ສະແດງວ່າ ຂາດການຕິດຕໍ
 socket.on('disconnect', function(){ console.log('disconnect');});
 //socket.emit('pi');
-//ອ່ານຄ່າຈາກ Server
-socket.on('pi', function(data){ console.log('read json data from server'); //console.log(data);
+//ອ່ານຄ່າຈາກດອກໄຟຟ້າຈາກ Server
+socket.on('pi', function(data){ console.log('read json data from server'); 
 //obj = JSON.parse(data);
   for(var i=0;i<data.length;i++){
 console.log(data[i]);
@@ -61,7 +60,6 @@ if(data.pin=="11"){gpio11.set(1);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="2"){gpio2.set(1);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="3"){gpio3.set(1);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="14"){gpio14.set(1);socket.emit('robot',{'ack':data.pin});};
-//if(data.pin=="21"){gpio21.set(1);socket.emit('robot',{'ack':data.pin});};
 };
 if(data.stt=="on")
 {
@@ -82,7 +80,5 @@ if(data.pin=="11"){gpio11.set(0);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="2"){gpio2.set(0);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="3"){gpio3.set(0);socket.emit('robot',{'ack':data.pin});};
 if(data.pin=="14"){gpio14.set(0);socket.emit('robot',{'ack':data.pin});};
-//if(data.pin=="21"){gpio21.set(0);socket.emit('robot',{'ack':data.pin});};
 };
-
 };
